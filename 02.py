bc7bd6e1909012df71ca3438ba0d663a4b5168d7
dionomusuko <dandybird1017@gmail.com>
2019-09-10 23:19:18 +0900

    add binary

---
+++ 02.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+
+n = int(input())
+m = int(input()) 
+
+result = n*6000 + m*4000
+
+print(result)
---
+++ paiza.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+
+one_line = int(input())
+secound_line = int(input('角度入力してください'))
+
+result = 180 - (one_line + secound_line)
+
+print(result)
